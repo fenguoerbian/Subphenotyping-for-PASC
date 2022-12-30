6dc78919de6f857515357445dd6e90c899bde50b
成超(Cheng Chao) <YJ220158@simcere.com>
2022-12-30 17:46:22 +0800

    Adjust the python script.

--- Python code for training topic modeling/Main_train_topic_model.py
+++ Python code for training topic modeling/Main_train_topic_model.py
@@ -12,13 +12,13 @@ Train topic model
 import numpy as np
 import scipy.io as sio
 from scipy.sparse import coo_matrix
-from pydpm.model import PFA
+from pydpm_bak.model import PFA
 
 data = sio.loadmat('../dataset/COVID_positive_any_new_PASC_Feature_DX.mat')
 X = np.array(data['Feature_DX'].T, order='C')
 
 K=10
 
-model=PFA(K, 'cpu')
+model=PFA(K, device = "cpu")
 model.initial(X)
 model.train(iter_all=1000, burn_in = 500)
